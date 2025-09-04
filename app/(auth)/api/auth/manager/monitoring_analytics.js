fe966a38c25158b3bdcddaac865536ab705a90fa
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-09-04 15:29:10 -0600

    Create monitoring_analytics.js

---
+++ app/(auth)/api/auth/manager/monitoring_analytics.js
@@ -0,0 +1,9 @@
+const apiPerformance = {
+  coinbase: { success: 0, failures: 0, avgResponseTime: 0 },
+  solana: { success: 0, failures: 0, avgResponseTime: 0 },
+  // ... other services
+};
+
+function trackAPIPerformance(service, success, responseTime) {
+  // Implementation to track and display API performance
+}
